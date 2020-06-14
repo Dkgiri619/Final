f9fd9a386399c05476b4c4abb842fcd9a4eb48cf
Dkg619 <dkgiri619>
2020-06-14 18:19:56 +0530

    modified contact

--- main/views.py
+++ main/views.py
@@ -11,11 +11,11 @@ def home(request):
         name= data.get("contact_name")
         email= data.get("contact_email")
         phone = data.get("contact_phone")
-        comment=data.get("content")
+        comment=data.get("comment")
          
         stuff = [name, email, comment, phone]
         
-        listToStr = ' '.join([str(elem) for elem in stuff])
+        listToStr = '\n '.join([str(elem) for elem in stuff])
 
         send_mail('recieved mail from website', listToStr , 'work4lanceindia@gmail.com' , ['work4lanceindia@gmail.com'], fail_silently=False )
         print(listToStr)
